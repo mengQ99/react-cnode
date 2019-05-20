decd9ce5c800cd51a66c7ef5e3e473b42e7f4de6
mengQ99 <m17854257603@163.com>
2019-05-20 21:44:06 +0800

    +

--- src/view/common/main-header.js
+++ src/view/common/main-header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Layout, Row, Col } from 'antd';
+import { Layout, Row, Col, Divider, Icon, Dropdown, Button } from 'antd';
+import NavMenu from './nav-menu';
 
 class MainHeader extends Component {
   render() {
@@ -7,11 +8,24 @@ class MainHeader extends Component {
       <Layout.Header>
         <Row>
           <Col md={6} xs={24}><h1 id="logo">cnode</h1></Col>
-          <Col md={18} xs={0}></Col>
+          <Col md={18} xs={0}>
+            <Divider type="vertical"></Divider>
+            <NavMenu mode="horizontal" className="header-nav"/>
+          </Col>
+          <Col md={0} xs={24} style={{ position: 'absolute', top: 0, right: 0, width: 'auto' }}>
+            <Dropdown 
+              overlay={( <NavMenu/> )} 
+              trigger={['click']}
+              placement="bottomLeft"
+            >
+              <Button type="link">nn
+                <Icon type="menu" style={{fontSize: 18, color: '#FFF'}}/>
+              </Button>
+            </Dropdown>
+          </Col>
         </Row>
       </Layout.Header>
     )
   }
 }
-
 export default MainHeader
