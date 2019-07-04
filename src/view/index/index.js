4fa291a23f38dc419fb4deb563df865d07e7a5f5
mengQ99 <m17854257603@163.com>
2019-07-04 19:07:35 +0800

    m

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -5,33 +5,30 @@ import IndexList from './list';
 
 export default class Index extends Component {
   render() {
-    console.log(this.props)
+    let tab = this.props.match.params.id
+    let menu = [
+      { title: '全部', key: 'all' },
+      { title: '精华', key: 'good' },
+      { title: '问题', key: 'ask' },
+      { title: '分享', key: 'share' },
+      { title: '招聘', key: 'job' },
+      { title: '测试', key: 'dev' },
+    ]
     return (
       <Row className="wrap">
         <Col md={6}>
-          <Menu id="indexMenu">
-            <Menu.Item>
-              <Link to="/index/all">全部</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/index/good">精华</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/index/ask">问题</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/index/share">分享</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/index/job">招聘</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/index/dev">测试</Link>
-            </Menu.Item>
+          <Menu id="indexMenu" defaultSelectedKeys={['all']} mode="inline">
+            {
+              menu.map(e => (
+                <Menu.Item key={e.key}>
+                  <Link to={"/index/"+ e.key}>{e.title}</Link>
+                </Menu.Item>
+              ))
+            }
           </Menu>         
         </Col>
         <Col md={18}>
-          <IndexList/>
+          <IndexList tab={tab}/>
         </Col>
       </Row>
 
