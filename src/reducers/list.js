6d04f3c4213a15cb3c0c9df2d7a1b605cb60ea6d
mengQ99 <m17854257603@163.com>
2019-07-04 19:06:50 +0800

    异步获取首页列表

--- src/reducers/list.js
+++ src/reducers/list.js
@@ -1,5 +1,21 @@
-const list = (state={}, action) => {
-  switch(action.type){
+const list = (state = { data: [], loading: true }, action) => {
+  // debugger
+  switch (action.type) {
+    case 'LIST_UPDATE': 
+      return {
+        loading: true,
+        data: state.data
+      }
+    case 'LIST_UPDATE_SUCCESS':
+      return {
+        loading: false,
+        data: action.data.data
+      }
+    case 'LIST_UPDATE_ERROR':
+      return {
+        loading: false,
+        data: []
+      }
     default: return state
   }
 }
--- src/view/index/list.js
+++ src/view/index/list.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { List, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import data from './data';
 import TxtTag from '../common/txt-tag'
+import axios from 'axios';
 
 
 
-export default class IndexList extends Component {
+class IndexList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: 1,
+    }
+    this.getData(this.props.tab)
+  }
+  //父组件render，子组件就会触发componentWillReceiveProps方法
+  componentWillReceiveProps(nextProps) {
+    if (this.props.tab != nextProps.tab) {
+      this.getData(nextProps.tab)
+    }
+  }
+
+  getData(tab) {
+    this.props.dispatch((dispatch) => {
+      dispatch({ type: 'LIST_UPDATE' })
+      axios.get(`https://cnodejs.org/api/v1/topics?tab=${tab}&page=${this.state.page}&limit=15`)
+        .then(res => {
+          dispatch({
+            type: 'LIST_UPDATE_SUCCESS',
+            data: res.data
+          })
+        })
+        .catch(err => {
+          dispatch({
+            type: 'LIST_UPDATE_ERROR',
+            data: err
+          })
+          console.log(err)
+        })
+    })
+  }
+
   render() {
+    console.log(this.props)
+    let { loading, data } = this.props
     return <List
-      loading={false}
-      dataSource={data.data}
+      loading={loading}
+      dataSource={data}
       renderItem={item => (
-        <List.Item 
-          actions={["回复：" + item.reply_count, "访问：" + item.visit_count]} 
+        <List.Item
+          actions={["回复：" + item.reply_count, "访问：" + item.visit_count]}
           key={item.id}
-          style={{paddingLeft: 12, borderLeft: '1px solid #e8e8e8'}}
+          style={{ paddingLeft: 12, borderLeft: '1px solid #e8e8e8' }}
         >
           <List.Item.Meta
             avatar={<Avatar src={item.author.avatar_url} />}
             title={
               <div>
-                <TxtTag data={item}/>
+                <TxtTag data={item} />
                 <Link to={"/details/" + item.id}>{item.title}</Link>
               </div>
             }
@@ -39,3 +77,5 @@ export default class IndexList extends Component {
     </List>
   }
 }
+
+export default connect(state => state.list)(IndexList)
