4a1aa3e6b1099d084922fd93e6b9ac3cf2fa963e
mengQ99 <m17854257603@163.com>
2019-07-10 20:31:40 +0800

    用户详情数据异步获取

--- src/reducers/details.js
+++ src/reducers/details.js
@@ -1,5 +1,35 @@
-const details = (state={}, action) => {
+const details = (state={
+  data: {
+    author: {},
+    replies: [],
+    reply_count: 0,
+    create_at: '',
+    good: true
+  },
+  loading: true
+}, action) => {
   switch(action.type){
+    case 'DETAILS_UPDATE': 
+      return {
+        data: state.data,
+        loading: true,
+      }
+    case 'DETAILS_UPDATE_SUCCESS':
+      return {
+        data: action.data.data,
+        loading: false
+      }
+    case 'DETAILS_UPDATE_ERROR':
+      return {
+        data: {
+          author: {},
+          replies: [],
+          reply_count: 0,
+          create_at: '',
+          good: true
+        },
+        loading: true
+      }
     default: return state
   }
 }
--- src/view/details/index.js
+++ src/view/details/index.js
@@ -3,40 +3,72 @@ import { Card, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import data from './data';
 import TxtTag from '../common/txt-tag';
-import Replies from './replies'
+import Replies from './replies';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 const res = data.data
 
-export default class Details extends Component {
+class Details extends Component {
+
+  constructor(props){
+    super(props)
+    let id = this.props.match.params.id
+    this.getData(id)
+  }
+
+  getData(id) {
+    this.props.dispatch((dispatch) => {
+      dispatch({
+        type: 'DETAILS_UPDATE'
+      })
+      axios.get(`https://cnodejs.org/api/v1/topic/${id}`)
+        .then(res => {
+          dispatch({
+            type: 'DETAILS_UPDATE_SUCCESS',
+            data: res.data
+          })
+        }).catch(err => {
+          dispatch({
+            type: 'DETAILS_UPDATE_ERROR'
+          })
+        })
+    })
+  }
+
   render() {
+    let { loading, data } = this.props
     return (
       <div className="wrap">
         <Card
+          loading={loading}
           title={(
             <div>
               <h1>{res.title}</h1>
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                <TxtTag data={res} />
-                <Avatar src={res.author.avatar_url} />&nbsp;&nbsp;
-                <Link to={'/user/' + res.author.loginname}>{res.author.loginname}</Link>
-                &nbsp;发表于&nbsp;{res.create_at.split('T')[0]}
+                <TxtTag data={data} />
+                <Avatar src={data.author.avatar_url} />&nbsp;&nbsp;
+                <Link to={'/user/' + data.author.loginname}>{data.author.loginname}</Link>
+                &nbsp;发表于&nbsp;{data.create_at.split('T')[0]}
               </div>
             </div>
           )}
         >
           <div
             dangerouslySetInnerHTML={{
-              __html: res.content
+              __html: data.content
             }}
           >
         </div>
         </Card>
         <Replies
-          loading={false}
-          replies={res.replies}
-          replyCount={res.reply_count}
+          loading={loading}
+          replies={data.replies}
+          replyCount={data.reply_count}
         />
       </div>
     )
   }
 }
+
+export default connect(state => state.details)(Details)
