9aaea3a042634c72c8e56885807f47cc46566418
mengQ99 <m17854257603@163.com>
2019-07-25 21:12:22 +0800

    顶部导航根据路由选中问题

--- src/view/common/nav-menu.js
+++ src/view/common/nav-menu.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Menu, Icon } from 'antd';
 
-export default class NavMenu extends Component {
+class NavMenu extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = { cur: this.getCur(this.props.location) }
+  }
+
+  getCur(location){
+    return location.pathname.split('/')[1]
+  }
+
+  shouldComponentUpdate(nextProps){
+    debugger
+    let cur = this.getCur(nextProps.location) 
+    if(cur !== this.state.cur) {
+      this.setState({cur})
+      return false
+    }
+    return true
+  }
+
   render(){
     let { mode, className } = this.props
     return (
-      <Menu mode={mode} className={className}>
-        <Menu.Item><Link to="/index/all"><Icon type="home"/>首页</Link></Menu.Item>
-        <Menu.Item><Link to="/book"><Icon type="book"/>教程</Link></Menu.Item>
-        <Menu.Item><Link to="/about"><Icon type="info-circle-o"/>关于</Link></Menu.Item>
+      <Menu mode={mode} className={className} selectedKeys={[this.state.cur]}>
+        <Menu.Item key="index"><Link to="/index/all"><Icon type="home"/>首页</Link></Menu.Item>
+        <Menu.Item key="book"><Link to="/book"><Icon type="book"/>教程</Link></Menu.Item>
+        <Menu.Item key="about"><Link to="/about"><Icon type="info-circle-o"/>关于</Link></Menu.Item>
       </Menu>
     )
   }
 }
+
+export default withRouter(NavMenu)
